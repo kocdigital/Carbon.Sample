a685bee2a0887f0d9092a4611223984832638036
anileladag <anil.eladag@kocdigital.com>
2024-12-13 14:04:32 +0300

    Feat: Add initial configuration files and update DTOs for improved data handling

--- Carbon.Sample.API/Application/Dto/SampleFilterDto.cs
+++ Carbon.Sample.API/Application/Dto/SampleFilterDto.cs
@@ -6,9 +6,9 @@ using System.Collections.Generic;
 
 namespace Carbon.Sample.API.Application.Dto
 {
-	public class SampleFilterDto : IOrderableData, IPageableData
+	public class SampleFilterDto : IOrdinatedPageDto
 	{
-		public IList<Guid> Ids { get; set; }
+		public List<string> IdList { get; set; } = new List<string>();
 		public string Name { get; set; }
 		public bool? IsActive { get; set; }
 		public Guid TenantId { get; set; }
--- Carbon.Sample.API/Application/Dto/Validators/SampleFilterDtoValidator.cs
+++ Carbon.Sample.API/Application/Dto/Validators/SampleFilterDtoValidator.cs
@@ -11,8 +11,8 @@ namespace Carbon.Sample.API.Application.Dto.Validators
 	{
 		public SampleFilterDtoValidator()
 		{
-			RuleFor(x => x.Ids).Must(x => x == null || !x.Any(y => y != Guid.Empty));
-			RuleFor(x => x.Name).NotEmpty();
+			RuleFor(x => x.IdList).Must(x => x == null || !x.Any(y => !string.IsNullOrWhiteSpace(y)));
+			//RuleFor(x => x.Name).NotEmpty();
 			RuleFor(x => x.TenantId).NotEmpty();
 		}
 	}
